bc05d75f81b4aebe568f1c9414fc2561cfc6427d
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2023-03-23 21:32:50 +0300

    android:id="@android:id/list" { liste parçası liste görünümünü tanıyacak }

---
+++ app/src/main/java/com/kirmizitech/listfragment/MyListFragment.kt
@@ -0,0 +1,20 @@
+package com.kirmizitech.listfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.ListFragment
+
+class MyListFragment : ListFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_list, container, false)
+    }
+
+}
