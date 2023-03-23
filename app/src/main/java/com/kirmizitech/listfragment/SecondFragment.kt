ebf055bbe9c2872756fcc235526f56beb5a244af
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2023-03-24 00:41:58 +0300

    city pictures added

--- app/src/main/java/com/kirmizitech/listfragment/SecondFragment.kt
+++ app/src/main/java/com/kirmizitech/listfragment/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 /**
  * A simple [Fragment] subclass.
@@ -14,12 +15,32 @@ import android.view.ViewGroup
 
 class SecondFragment : Fragment() {
 
+    lateinit var imageView: ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        /**
+        We're not going to be able to use the fine viw by I.D. function directly.
+         */
+        val view: View = inflater.inflate(R.layout.fragment_second, container, false)
+        imageView = view.findViewById(R.id.imageView)
+
+        val position = arguments?.getInt("position", 0)
+
+        when (position) {
+            0 -> imageView.setImageResource(R.drawable.berlin)
+            1 -> imageView.setImageResource(R.drawable.athens)
+            2 -> imageView.setImageResource(R.drawable.rome)
+            3 -> imageView.setImageResource(R.drawable.tokyo)
+            4 -> imageView.setImageResource(R.drawable.amsterdam)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        //return inflater.inflate(R.layout.fragment_second, container, false)
+        return view
     }
 
 }
