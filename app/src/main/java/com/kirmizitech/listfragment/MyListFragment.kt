dba413e296a91d88827b9fab46f8b004d2ae5d2d
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2023-03-23 22:01:47 +0300

    Wrap with '?.let { ... }' call

--- app/src/main/java/com/kirmizitech/listfragment/MyListFragment.kt
+++ app/src/main/java/com/kirmizitech/listfragment/MyListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.ListFragment
 
 class MyListFragment : ListFragment() {
@@ -13,8 +14,24 @@ class MyListFragment : ListFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val arrayAdapter = ArrayAdapter.createFromResource(activity,R.array.cities,android.R.layout.simple_list_item_1)
+        //val arrayAdapter = ArrayAdapter.createFromResource(requireActivity(),R.array.cities,android.R.layout.simple_list_item_1)
+
+        //Wrap with '?.let { ... }' call
+        //önce etkinlik parametresi kontrol edilecek ve boş değilse {içerisindeki kod çalışacaktır}
+        val arrayAdapter = activity?.let {
+            ArrayAdapter.createFromResource(it,R.array.cities,android.R.layout.simple_list_item_1) }
+
     }
 
 }
